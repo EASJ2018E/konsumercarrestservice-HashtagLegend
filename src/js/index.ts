2644b77c968d9f6091846924aaa419fc5fcc8ed9
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-10-24 11:25:35 +0200

    added axios get commands

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,41 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {AxiosResponse, AxiosError} from "../../node_modules/axios/index";
+
+//http://rest-pele-easj-dk.azurewebsites.net/api/cars
+
+interface ICar{
+    model: string;
+    vendor: string;
+    price: number;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let getCarsButton: HTMLButtonElement = document.getElementById("getAll") as HTMLButtonElement
+getCarsButton.addEventListener("click", getAllCars)
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let content: HTMLDivElement = document.getElementById("content") as HTMLDivElement
+
+function getAllCars(): void{
+let uri: string = "http://rest-pele-easj-dk.azurewebsites.net/api/cars222";
+
+//axios.get <typen der returneres, kan evt være et array[]> (indsæt uri hvor data ligger)
+axios.get<ICar[]>(uri)
+//så laves der en funktion der har en parameter "response" af typen AxiosResponse, der modtager et ICar array.
+.then(function(response: AxiosResponse<ICar[]>):void{
+    let result: string = "<ol>"
+    //
+    response.data.forEach((car : ICar) => {
+        result += "<li> <b>model:</b> " + car.model + " <b>Vendor: </b>" + car.vendor + "<b>Price: </b>" + car.price + "</li>"
+    })
+        result += "</ol>"
+
+        content.innerHTML = result;
+    
+
+})
+
+//hvis der sker en fejl, så udskriver div tagget en fejl besked i stedet.
+.catch(function(error: AxiosError) :void{
+    content.innerHTML = error.message
+    
+})
+}
+
