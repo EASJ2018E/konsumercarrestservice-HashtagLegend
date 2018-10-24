e22bf3573184d245aa28d3195ba0939510c10b30
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-10-24 12:40:54 +0200

    added buttons for add car

--- src/js/index.ts
+++ src/js/index.ts
@@ -14,7 +14,7 @@ getCarsButton.addEventListener("click", getAllCars)
 let content: HTMLDivElement = document.getElementById("content") as HTMLDivElement
 
 function getAllCars(): void{
-let uri: string = "http://rest-pele-easj-dk.azurewebsites.net/api/cars222";
+let uri: string = "http://rest-pele-easj-dk.azurewebsites.net/api/cars";
 
 //axios.get <typen der returneres, kan evt være et array[]> (indsæt uri hvor data ligger)
 axios.get<ICar[]>(uri)
@@ -29,13 +29,13 @@ axios.get<ICar[]>(uri)
 
         content.innerHTML = result;
     
-
 })
 
 //hvis der sker en fejl, så udskriver div tagget en fejl besked i stedet.
 .catch(function(error: AxiosError) :void{
     content.innerHTML = error.message
-    
 })
+
+
 }
 
